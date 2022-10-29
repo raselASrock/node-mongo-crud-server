2cbe13cae4c82f9edd0d623cdb49555d44137617
raSel <mdraselm413@gmail.com>
2022-10-29 23:20:04 +0600

    simple-server

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.port || 5000;
 
+
+// middleware setup
+app.use(cors());
+app.use(express.json());
+
+
+// async await
+async function run(){
+
+}
+
+run().catch(err => console.log(err))
+
 app.get("/", (req, res) =>{
-    res.send("Simple Node Server Running....");
+    res.send("Hello from mongo crud server....");
 });
 
 app.listen(port, () =>{
-    console.log(`Simple Node Server Running On Port Number: ${port}`);
+    console.log(`Listening Node Server Running On Port Number: ${port}`);
 })
